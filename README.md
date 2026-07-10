21ff28437316453854b94dbd1c10035da8a91129
Santiago Muñoz <munozgonzalezsantiago@gmail.com>
2026-07-10 12:03:46 +0200

    initial setup app.py

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # mlops-bootcamp-mlflow-aws
 
-Idea: use MLflow hosted in AWS EC2 instance
+Idea: use MLflow hosted in AWS EC2 instance (only MLflow, not the whole data science project)
 
---
+++ app.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+import pandas as pd
+import numpy as np
+
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+
+import mlflow
+from mlflow.models.signature import infer_signature
+import mlflow.sklearn
+from urllib.parse import urlparse
+
+import logging
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+
+
+
+if __name__ == "__main__":
+    pass
