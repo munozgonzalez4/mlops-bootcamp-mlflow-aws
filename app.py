5a342302ac963f925c35dfca17f56a6ece70b91c
Santiago Muñoz <munozgonzalezsantiago@gmail.com>
2026-07-10 12:30:55 +0200

    app.py: first basic code without configuring AWS server

--- app.py
+++ app.py
@@ -26,7 +26,61 @@ def eval_metrics(actual, pred):
     return rmse, mae, r2
 
 
-
-
 if __name__ == "__main__":
-    pass
+    
+    # Data ingestion - wine quality dataset
+    csv_url = (
+        "https://raw.githubusercontent.com/mlflow/mlflow/master/tests/datasets/winequality-red.csv"
+    )
+    try:
+        data = pd.read_csv(csv_url, sep=";")
+    except Exception as e:
+        logger.exception(
+            "Unable to download training & test CSV, check your internet connection. Error: %s", e
+        )
+    
+    # Split the data into training and test sets. (0.75, 0.25) split.
+    train, test = train_test_split(data)
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+    train_y = train[["quality"]]
+    test_y = test[["quality"]]
+
+    # Hyperparameters for ElasticNet model
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+
+    with mlflow.start_run():
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr.fit(train_x, train_y)
+
+        predicted_qualities = lr.predict(test_x)
+
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+        print("ElasticNet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+        print("  RMSE: %s" % rmse)
+        print("  MAE: %s" % mae)
+        print("  R2: %s" % r2)
+
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
+
+        # Setup for the remote AWS server
+        remote_server_uri = ""
+        mlflow.set_tracking_uri(remote_server_uri)
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+        if tracking_url_type_store != "file":
+            mlflow.sklearn.log_model(
+                lr, 
+                "model", 
+                registered_model_name="ElasticnetWineModel"
+            )
+        else:
+            mlflow.sklearn.log_model(
+                lr, 
+                "model"
+            )
